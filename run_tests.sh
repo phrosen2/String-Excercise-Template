186a89e0b6b9fe3dd07dffe5a708bfbbe15cda66
phrosen2 <69522417+phrosen2@users.noreply.github.com>
2026-04-21 12:47:11 -0500

    Refactor run_tests.sh to use JUnit for testing
    Updated the test script to use JUnit for running tests and removed the old test execution logic.

--- run_tests.sh
+++ run_tests.sh
@@ -2,58 +2,13 @@
 
 set -e
 
-javac *.java
+echo "Compiling..."
 
-score=0
-total=0
+javac -cp .:junit-platform-console-standalone.jar StringExcerises.java StringExercisesTest.java
 
-for i in tests/*input.txt; do
-    num=$(basename $i | grep -o '[0-9]*')
-    expected="tests/${num}_expected.txt"
-    total=$((total + 1))
+echo "Running tests..."
 
-    actual=$(java TempConverter < "$i")
-
-    # check for contains instead of exact match (careful with newlines)
-    if grep -q -f $expected <(echo "$actual"); then
-        echo "Test $num: PASS"
-        score=$((score + 1))
-    else
-        cat $expected
-        echo ""
-        echo "Test $num: FAIL"
-        echo "Could not find $expected in output."
-        echo "Student output was:"
-        echo "$actual"
-    fi
-
-
-    # diff needs the --strip-trailing-cr to deal with Unix/Windows confusion in output
-   # if diff --strip-trailing-cr <(echo "$actual") "$expected" > /dev/null; then
-    #    echo "Test $num: PASS"
-     #   score=$((score + 1))
- #   else
-  #      echo "Test $num: FAIL"
-   #     echo "Expected: "
-    #    cat $expected
-     #   echo "Actually got:"
-      #  echo $actual
-   # fi
-done
-
-#Code Checks
-echo "Checking Scanner..."
-total=$((total + 1))
-if grep -q "Scanner" TempConverter.java; then
-    echo "Scanner: PASS (+1)"
-    score=$((score + 1))
-else
-    echo "Scanner: FAIL"
-fi
-
-
-# Final score
-echo "----------------------"
-echo "SCORE: $score / $total"
-echo "----------------------"
-
+java -jar junit-platform-console-standalone.jar \
+  --class-path . \
+  --scan-class-path \
+  --details summary
